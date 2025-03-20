f02b18e55c5512e57bf3c05141bd313cfeecba45
admin <admin@ZMIEN-MNIE>
2025-03-20 13:22:05 +0100

    .

--- konsola1/Program.cs
+++ konsola1/Program.cs
@@ -2,6 +2,10 @@
 
 class Program
 {
+    /// <summary>
+    /// metoda programu. Generuje tablicę, pobiera wartość do wyszukania od użytkownika.Przeszukuje tablicę i wyświetla wyniki.
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         int[] array = GenerateRandomArray(50, 1, 100);
@@ -22,8 +26,14 @@ class Program
         else
         {
             Console.WriteLine("Nieprawidłowa wartość wejściowa.");
-        }
-    }
+        }}
+    /// <summary>
+    ///  Generuje tablicę liczb losowych.
+    /// </summary>
+    /// <param name="size">Rozmiar tablicy.</param>
+    /// <param name="minValue">Minimalna wartość </param>
+    /// <param name="maxValue">Maksymalna wartość</param>
+    /// <returns></returns>
     static int[] GenerateRandomArray(int size, int minValue, int maxValue)
     {
         Random random = new Random();
@@ -34,13 +44,18 @@ class Program
         }
         return array;
     }
+    /// <summary>
+    /// Przeszukuje tablice.
+    /// </summary>
+    /// <param name="array">tablica do przeszukania</param>
+    /// <param name="value">wartość do wyszukania</param>
+    /// <returns>Indeks znalezionego elementu lub długość tablicy,jeśli element nie został znaleziony</returns>
     static int SearchWithSentinel(int[] array, int value)
     {
         int n = array.Length;
         int[] extendedArray = new int[n + 1];
         Array.Copy(array, extendedArray, n);
         extendedArray[n] = value; 
-
         int i = 0;
         while (extendedArray[i] != value)
         {
